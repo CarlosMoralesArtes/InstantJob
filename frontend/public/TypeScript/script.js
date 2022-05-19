02e26f4944909a8dd84479099233b20f29765c54
carlos <carlos.morales.artes@gmail.com>
2022-05-19 08:26:30 +0200

    Primera plana Plantilla

--- frontend/public/TypeScript/script.js
+++ frontend/public/TypeScript/script.js
@@ -1,1 +1,35 @@
-alert("hola");
+function obrir(){
+    var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+        overlay = document.getElementById('overlay'),
+        popup = document.getElementById('popup'),
+        btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+
+    btnAbrirPopup.addEventListener('click', function(){
+        overlay.classList.add('active');
+        popup.classList.add('active');
+    });
+
+    btnCerrarPopup.addEventListener('click', function(e){
+        e.preventDefault();
+        overlay.classList.remove('active');
+        popup.classList.remove('active');
+    });
+}
+
+function obrir2(){
+    var btnAbrirPopup = document.getElementById('btn-abrir-popup2'),
+        overlay = document.getElementById('overlay2'),
+        popup = document.getElementById('popup2'),
+        btnCerrarPopup = document.getElementById('btn-cerrar-popup2');
+
+    btnAbrirPopup.addEventListener('click', function(){
+        overlay.classList.add('active');
+        popup.classList.add('active');
+    });
+
+    btnCerrarPopup.addEventListener('click', function(e){
+        e.preventDefault();
+        overlay.classList.remove('active');
+        popup.classList.remove('active');
+    });
+}
