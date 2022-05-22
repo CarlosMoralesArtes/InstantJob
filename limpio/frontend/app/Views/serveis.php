96d785f215394be58eafc86d5458baff80d0db02
Ericson246 <ericgarciaju@gmail.com>
2022-05-22 22:39:57 +0200

    Inicio sesion bonito

---
+++ limpio/frontend/app/Views/serveis.php
@@ -0,0 +1,159 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <title>InstantJob | Home</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="styles.css">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+  <script src="Typescript/script.js"></script>
+
+  <!-- Bootstrap CSS v5.0.2 -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <nav class="navIniciServeis">
+      <header class="headerServeis">
+        <h1>Nom categoria</h1>
+        <h2>Numero de serveis</h2>
+      </header>
+    </nav>
+    <br>
+    <h2>Serveis Destacats</h2><br>
+    <div class="serveiSeparat2 col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <br>
+    <div class="serveiSeparat2 col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+  <div>
+    <h2>Mes Serveis</h2><a href="#" class="btn btn-primary">Preu</a><a href="#" class="btn btn-primary">Nom</a><br>
+    <div class="serveiSeparat2 col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <br>
+    <div class="serveiSeparat2 col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+    <div class="serveiSeparat col-4" style="width: 18rem;">
+      <img src="./imgs/imatgePre.png" width="100%" height="150px">
+      <div class="card-body">
+        <p class="card-title">Fontaneria</p>
+        <p class="card-title">Número</p>
+        <p class="card-text"></p>
+      </div>
+    </div>
+  </div>
+
+  <footer>
+    <div class="footer-content col-4">
+      <img src="./imgs/Logo_InstantJob_Blanca.png">
+      <p>Servei Tècnic</p>
+      <p>Tel. 99 999 999</p>
+    </div>
+    <div class="footer-content col-4">
+      <p>Informació Legal</p>
+      <p>Avís Legal</p>
+      <p>Política de Privacitat</p>
+      <p>Política de Cookies</p>
+    </div>
+    <!-- Categories que s'agafaran de typescript -->
+    <div class="footer-content col-3">
+      <p>Categories</p>
+      <p>Avís Legal</p>
+      <p>Política de Privacitat</p>
+      <p>Política de Cookies</p>
+    </div>
+  </footer>
+
+  <!-- Bootstrap JavaScript Libraries -->
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+    integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+    integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF"
+    crossorigin="anonymous"></script>
+</body>
+</html>
